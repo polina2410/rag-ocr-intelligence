f975d1060eea4c7d5fd0d52aaaab369c80ec217c
Polina Smekhova <polinasmekhova@gmail.com>
2026-06-13 16:17:32 +0200

    feat: ObstacleSplit TypeORM entity mapped to obstacle_splits table

---
+++ apps/backend/src/entities/obstacle-split.entity.ts
@@ -0,0 +1,39 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { RaceResult } from './race-result.entity';
+
+const OBSTACLE_NAME_MAX_LENGTH = 255;
+
+@Entity('obstacle_splits')
+export class ObstacleSplit {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @ManyToOne(() => RaceResult)
+  @JoinColumn({ name: 'race_result_id' })
+  raceResult!: RaceResult;
+
+  @Column({ type: 'uuid', name: 'race_result_id' })
+  raceResultId!: string;
+
+  @Column({ type: 'int', name: 'obstacle_number' })
+  obstacleNumber!: number;
+
+  @Column({
+    type: 'varchar',
+    length: OBSTACLE_NAME_MAX_LENGTH,
+    name: 'obstacle_name',
+  })
+  obstacleName!: string;
+
+  @Column({ type: 'int', name: 'split_time_seconds', nullable: true })
+  splitTimeSeconds!: number | null;
+
+  @Column({ type: 'int', name: 'penalty_count', default: 0 })
+  penaltyCount!: number;
+}
